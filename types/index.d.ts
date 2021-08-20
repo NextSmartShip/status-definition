4f1e57de5f12a517c2c15de33b3d7131ed9a1aa6
Will <will@fingersonezero.com>
2021-08-20 15:09:25 +0800

    add status definition types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,6 +1,10 @@
+import { Channel } from "./channel";
+import { CourierOrder } from "./courierOrder";
 import { Inbound } from "./inbound";
 import { Order } from "./order";
+import { PickingOrder } from "./pickingOrder";
 import { Product } from "./product";
+import { Shelve } from "./shelve";
 import { Unshelve } from "./unshelve";
 
 export const order: Order;
@@ -10,3 +14,11 @@ export const inbound: Inbound;
 export const product: Product;
 
 export const unshelve: Unshelve;
+
+export const channel: Channel;
+
+export const courierOrder: CourierOrder;
+
+export const pickingOrder: PickingOrder;
+
+export const shelve: Shelve;
