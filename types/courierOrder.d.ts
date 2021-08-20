803c8cc9c556172bb4bedaec050463b4d58d9854
Will <will.zhang@nextsmartship.com>
2021-08-20 18:29:18 +0800

    Fixed courier order type

--- types/courierOrder.d.ts
+++ types/courierOrder.d.ts
@@ -1,4 +1,4 @@
-export type CourierOrderStatus = 'NEW' | 'READY' | 'FAILED' 
+export type CourierOrderStatus = 'NEW' | 'READY' | 'FAILED' | 'SUCCESS' | 'COMPLETED' | 'CANCELED'
 
 export interface CourierOrder {
   status: Record<CourierOrderStatus, number>;
