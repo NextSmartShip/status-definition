076f45601198be634742c363399680961aa53c18
Will <will.zhang@nextsmartship.com>
2021-08-21 11:13:15 +0800

    update index.d.ts path

---
+++ index.d.ts
@@ -0,0 +1,24 @@
+import { Channel } from "./types/channel";
+import { CourierOrder } from "./types/courierOrder";
+import { Inbound } from "./types/inbound";
+import { Order } from "./types/order";
+import { PickingOrder } from "./types/pickingOrder";
+import { Product } from "./types/product";
+import { Shelve } from "./types/shelve";
+import { Unshelve } from "./types/unshelve";
+
+export const order: Order;
+
+export const inbound: Inbound;
+
+export const product: Product;
+
+export const unshelve: Unshelve;
+
+export const channel: Channel;
+
+export const courierOrder: CourierOrder;
+
+export const pickingOrder: PickingOrder;
+
+export const shelve: Shelve;
