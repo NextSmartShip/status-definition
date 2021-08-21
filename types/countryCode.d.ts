46a0feaa8e65bc2790dcbfb6c56636fd9a3fa769
Will <will@fingersonezero.com>
2021-08-21 11:56:53 +0800

    Add cycleCount and countryCode statusLabels

--- types/countryCode.d.ts
+++ types/countryCode.d.ts
@@ -1,5 +1,8 @@
+import { LabelDataType } from "./common"
+
 export type CountryCodeStatus = "Operating" | "Suspended"
 
 export type CountryCode = {
   status: Record<CountryCodeStatus, number>
+  statusLabels: LabelDataType[]
 }
--- types/cycleCount.d.ts
+++ types/cycleCount.d.ts
@@ -1,5 +1,8 @@
+import { LabelDataType } from "./common"
+
 export type CycleCountStatus = "New" | "SendFailed" | "SendSuccess" | "ConfirmFailed" | "Confirm"
 
 export type CycleCount = {
   status: Record<CycleCountStatus, number>
+  statusLabels: LabelDataType[]
 }
